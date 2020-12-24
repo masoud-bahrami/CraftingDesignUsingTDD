022874ea38427b6f726191a0fc5d7dfa75f70fa8
Masoud Bahrami <mbahrami1369@live.com>
2020-12-24 05:44:23 +0330

    xUnit Tests

---
+++ TestFrameworks/XUnit/CalculatorDataHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ExUnit
+{
+    public class CalculatorDataHelper : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 50, 50, 100 };
+            yield return new object[] { 100, 300, 400 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ TestFrameworks/XUnit/CalculatorTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleCalculator;
+using Xunit;
+
+namespace ExUnit
+{
+    public class CalculatorFixture : IDisposable
+    {
+        protected Calculator sut;
+
+        public CalculatorFixture()
+        {
+            sut = new Calculator();
+        }
+
+
+        public void Dispose()
+        {
+            sut.Dispose();
+        }
+    }
+    public class CalculatorTests : CalculatorFixture, IClassFixture<CalculatorFixture>
+    {
+        [Fact]
+        public void TestSum()
+        {
+            var augend = 5;
+            var addend = 6;
+            var expectedResult = 11;
+
+            var result = sut.Sum(augend, addend);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> GetSumData()
+        {
+            yield return new object[] { 10, 20, 30 };
+            yield return new object[] { 10, 30, 40 };
+        }
+
+        [Theory]
+        [InlineData(5, 6, 11)]
+        [InlineData(5, 10, 15)]
+        [MemberData(nameof(GetSumData))]
+        [ClassData(typeof(CalculatorDataHelper))]
+        public void TestSum_Parameterized(int augend, int addend, int expectedResult)
+        {
+            var result = sut.Sum(augend, addend);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void TestDivisionByZero()
+        {
+            var numerator = 5;
+            var denominator = 0;
+
+            Assert.ThrowsAny<DivideByZeroException>(() => sut.Division(numerator, denominator));
+        }
+    }
+}
